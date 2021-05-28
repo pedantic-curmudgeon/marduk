ca6eb7099719b56d99632b1b6c99e19d1636a64c
raegancbarker <raegancbarker@gmail.com>
2021-05-28 12:47:58 -0500

    Pointed database engines to local .env for test database credentials.
    Updated database engines to use database credentials imported from
    the .env.test.local file.
    
    Updated requirements.txt to include new python-dotenv dependency.

--- database/engines.py
+++ database/engines.py
@@ -1,15 +1,26 @@
 """Standard database engines."""
 
+from dotenv import dotenv_values
+from pathlib import Path
 from sqlalchemy import create_engine
 
 
+# Get database environment variables.
+this_folder = Path(__file__).parent
+root_folder = this_folder.parent
+env_file = root_folder / 'docker' / '.env.test.local'
+env_dict = dotenv_values(env_file)
+db_name = env_dict['DB_NAME']
+db_user = env_dict['DB_USER']
+db_password = env_dict['DB_PASSWORD']
+
 # Define database engine for Docker Compose connectivity.
 compose_db_cfg = {
-    'user': 'root',
-    'password': 'my-secret-pw',
+    'user': db_user,
+    'password': db_password,
     'server': 'db_container', # This corresponds to the name of the DB container in the compose file.
     'port': '3306',
-    'db': 'main',
+    'db': db_name,
     'charset': 'utf8mb4'
 }
 
@@ -21,11 +32,11 @@ compose_db = create_engine(
 
 # Define database engine for local Docker Container connectivity.
 container_db_cfg = {
-    'user': 'root',
-    'password': 'my-secret-pw',
+    'user': db_user,
+    'password': db_password,
     'server': 'localhost',
     'port': '3306',
-    'db': 'main',
+    'db': db_name,
     'charset': 'utf8mb4'
 }
 
