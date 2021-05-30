a2989752f211c7d09fb5719b724fa737343c5324
raegancbarker <raegancbarker@gmail.com>
2021-05-30 10:31:28 -0500

    Updated to reference DB container name from .env.

--- database/engines.py
+++ database/engines.py
@@ -13,12 +13,13 @@ env_dict = dotenv_values(env_file)
 db_name = env_dict['DB_NAME']
 db_user = env_dict['DB_USER']
 db_password = env_dict['DB_PASSWORD']
+db_container = env_dict['DB_CONTAINER_NAME']
 
 # Define database engine for Docker Compose connectivity.
 compose_db_cfg = {
     'user': db_user,
     'password': db_password,
-    'server': 'db_container', # This corresponds to the name of the DB container in the compose file.
+    'server': db_container, # This corresponds to the name of the DB container in the compose file.
     'port': '3306',
     'db': db_name,
     'charset': 'utf8mb4'
